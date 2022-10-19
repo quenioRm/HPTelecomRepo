5f85633bd1b2fac67e45e170880d786b352ac45d
emers0mn <emeron0000@gmail.com>
2022-10-18 21:11:26 -0300

    att css

--- linktree/src/components/plans/plansPage.js
+++ linktree/src/components/plans/plansPage.js
@@ -29,149 +29,151 @@ export const PlansPage = () => {
     const [plans, setPlans] = useState([])
 
     useEffect(() => {
-        async function Get(){
+        async function Get() {
             await Api.get("/Web/GetPlans/10")
-            .then((response) =>{
-                console.log(response.data)
-                setPlans(response.data)
-            })
-            .catch((err) => {
-    
-            });
+                .then((response) => {
+                    console.log(response.data)
+                    setPlans(response.data)
+                })
+                .catch((err) => {
+
+                });
         }
         Get()
-    },[])
+    }, [])
 
-    return(
+    return (
         <>
-        <main className="content">
-            <div className="container text-center">
-                <div className="packages"> 
-                <Slider {...settings}>
-                {plans.map((item, key) => (
-                  <div key={key} className="col-lg-3 col-md-4 col-sm-6 col-xs-12 package-list">
-                    <div className="package-icon" >
-                        <img
-                        src={"/assets/images/" + item.icon}
-                        alt="velocimentro"
-                        className="img-fluid"
-                        />
-                    </div>
-                    <div className="package-info">
-                        <h1 style={{fontFamily:"Gordita-Bold"}} className="font-weight-bold">{item.name}</h1>
-                        <div className="desc">
-                        <ul style={{fontFamily:"Gordita-Regular"}} className="p-0 m-0 m-auto">
-                            <li>
-                            <i className="fa fa-download" />
-                            <strong>Download {item.downSpeed} Mbps</strong>
-                            </li>
-                            <li style={{fontFamily:"Gordita-Light"}}>
-                            <i className="fa fa-arrow-up" />
-                            Upload {item.upSpeed} Mbps
-                            </li>
-                            <br/>
-                            <li>
-                                <img
-                                    src="./assets/images/nofidelity.png"
-                                    alt="image"
-                                    className="img-fluid d-inline-block"
-                                    style={{ paddingRight: 10 }}
-                                />
-                                {item.description1}
-                            </li>
-                            
-                            <li>
-                            <i className="fa fa-wifi" />
-                            {item.description2}
-                            </li>
-
-                            <li>
-                            <i className="fa fa-credit-card" />
-                            {item.description4}
-                            </li>
-                        </ul>
-                        </div>
-                        <div class="line-divisor"></div>
-
-                        <div class="container-new-list">
-                                        <div class="container-bt">
-                                        </div>
-                                        <ul style={{listStyle:"none"}}>
-
-                                            <li>
-                                                <div class="list-bt">
-                                                    <input id="btn1" type="checkbox" name="" value=""></input>
-                                                    Apenas internet
-                                                </div>
-                                                
-                                            </li>
-
-                                            <br />
-
-                                            <li>
-                                                <div class="list-bt">
-                                                    <input id="btn1" type="checkbox" name="" value=""></input>
-                                                    Telefone ilimitado
-                                                </div>
-                                                (<span class="bonus">Ganhe + X Mega</span>)
-
-                                            </li>
-
-                                            <br />
-
-                                            <li>
-                                                <div class="list-bt" >
-                                                    <input id="btn1" type="checkbox" name="" value=""></input>
-                                                    Tv ( 91 canais) + VOD <br />
-
-                                                </div>
-                                                (<span class="bonus">Ganhe + X Mega</span>)
-                                            </li>
-
-                                            <br />
-
-                                            <li>
-
-                                            <div class="list-bt" >
-                                                     
-                                                         <div class="content-turbo">
-                                                             <div class="icon-turbo">
-                                                                 <img hidde class="turbo" src="./assets/images/fire.gif" />
-                                                                 <img class="turbo icon-velocidade" src="./assets/images/turbo.svg" />
-                                                             </div>
-                                                         
-                                                    <div>Turbo das 1h as 6h
-                                                        <br />
-                                                        (<span class="bonus">999 mega no Turbo</span>)
-                                                        <br />
-                                                    (<span class="bonus">saiba mais</span>)
-                                                    </div>
-                                                    
-                                                     </div>
-
-                                                </div>
-                                            </li>
-
-                                        </ul>
+            <main className="content">
+                <div className="container text-center">
+                    <div className="packages">
+                        <Slider {...settings}>
+                            {plans.map((item, key) => (
+                                <div key={key} className="col-lg-3 col-md-4 col-sm-6 col-xs-12 package-list">
+                                    <div className="package-icon" >
+                                        <img
+                                            src={"/assets/images/" + item.icon}
+                                            alt="velocimentro"
+                                            className="img-fluid"
+                                        />
                                     </div>
-                        <p style={{fontFamily:"Gordita-Light"}}>{item.addTex}</p>
+                                    <div className="package-info">
+                                        <h1 style={{ fontFamily: "Gordita" }} className="font-weight-bold">{item.name}</h1>
+                                        <div className="desc">
+                                            <ul style={{ fontFamily: "Gordita-Regular" }} className="p-0 m-0 m-auto">
+                                                <li>
+                                                    <i className="fa fa-download" />
+                                                    <strong>Download {item.downSpeed} Mbps</strong>
+                                                </li>
+                                                <li style={{ fontFamily: "Gordita-Light" }}>
+                                                    <i className="fa fa-arrow-up" />
+                                                    Upload {item.upSpeed} Mbps
+                                                </li>
+                                                <br />
+                                                <li>
+                                                    <img
+                                                        src="./assets/images/nofidelity.png"
+                                                        alt="image"
+                                                        className="img-fluid d-inline-block"
+                                                        style={{ paddingRight: 10 }}
+                                                    />
+                                                    {item.description1}
+                                                </li>
+
+                                                <li>
+                                                    <i className="fa fa-wifi" />
+                                                    {item.description2}
+                                                </li>
+
+                                                <li>
+                                                    <i className="fa fa-credit-card" />
+                                                    {item.description4}
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div class="line-divisor"></div>
+
+                                        <div class="container-new-list">
+                                            <div class="container-bt">
+                                            </div>
+                                            <ul>
+
+                                                <li>
+                                                    <div class="list-bt" style={{ fontFamily: "Gordita", fontWeight: "500"}}>
+                                                        <input id="btn1" type="checkbox" name="" value=""></input>
+                                                        Apenas internet
+                                                    </div>
+
+                                                </li>
+
+                                                <br />
+
+                                                <li>
+                                                    <div class="list-bt" style={{ fontFamily: "Gordita", fontWeight: "500"}}>
+                                                        <input id="btn1" type="checkbox" name="" value=""></input>
+                                                        Telefone ilimitado
+                                                    </div>
+                                                    (<span class="bonus">Ganhe + X Mega</span>)
+
+                                                </li>
+
+                                                <br />
+
+                                                <li>
+                                                    <div class="list-bt" style={{ fontFamily: "Gordita", fontWeight: "500"}}>
+                                                        <input id="btn1" type="checkbox" name="" value=""></input>
+                                                        Tv (91 canais) + VOD <br />
+
+                                                    </div>
+                                                    (<span class="bonus">Ganhe + X Mega</span>)
+                                                </li>
+
+                                                <br />
+
+                                                <li>
+
+                                                    <div class="list-bt" >
+
+                                                        <div class="content-turbo">
+                                                            <div class="icon-turbo">
+                                                                <img hidde class="turbo" src="./assets/images/fire.gif" />
+                                                                <img class="turbo icon-velocidade" src="./assets/images/turbo.svg" />
+                                                            </div>
+
+                                                            <div><span style={{ fontFamily: "Gordita", fontWeight: "500"}}>Turbo das 1h as 6h</span> <br /> (<span class="bonus">999 mega no Turbo</span>)
+                                                                <br />
+                                                                (<span class="bonus">saiba mais</span>)
+                                                            </div>
+
+                                                        </div>
+
+                                                    </div>
+                                                </li>
+
+                                            </ul>
+                                        </div>
+                                        <p style={{ fontFamily: "Gordita", fontWeight: "500", fontSize: "14px" }}>Pagando até o vencimento <br />  você ganha R$10 de desconto</p>
+                                    </div>
+                                    <div className="package-price">
+                                        <span className="sale">R$ {item.price.toFixed(2).replace('.', ',')}</span>
+                                        <span style={{ fontFamily: "Gordita", fontWeight: "900", color: "#008D1E", fontSize:"40px" }} className="price">
+                                            R${(item.price - item.discount).toFixed(2).replace('.', ',')}<span style={{ color: "#000", fontWeight: "500" }}>/mês</span>
+                                        </span>
+                                    </div>
+                                    <a href="https://api.whatsapp.com/send?phone=551128762641" target="_blank" className="button">
+                                        Contratar
+                                    </a>
+
+                                    <a href="#">
+                                        <h6>(<span className="channel">Confira todos os canais</span>)</h6>
+                                    </a>
+
+                                </div>
+                            ))}
+                        </Slider>
                     </div>
-                    <div className="package-price">
-                        <span className="sale">R$ {item.price.toFixed(2).replace('.', ',')}</span>
-                        <span style={{fontFamily:"Gordita"}} className="price">
-                            R${(item.price - item.discount).toFixed(2).replace('.', ',')}<span>/mes</span>
-                        </span>
-                        <h6 className="font-weight-light">No Boleto via e-mail</h6>
-                    </div>
-                    <a href="https://api.whatsapp.com/send?phone=551128762641" target="_blank" className="button">
-                        Contratar
-                    </a>
-                    </div>
-                ))}
-                </Slider>
                 </div>
-            </div>
-        </main>
+            </main>
         </>
     )
 }
